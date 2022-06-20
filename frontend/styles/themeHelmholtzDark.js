135ea39279376819bbfce1613da948e6c4b754a6
Marc Hanisch <mhanisch@gfz-potsdam.de>
2022-06-20 11:36:00 +0200

    add / adapt licence header

--- frontend/styles/themeHelmholtzDark.js
+++ frontend/styles/themeHelmholtzDark.js
@@ -1,3 +1,12 @@
+// SPDX-FileCopyrightText: 2022 Dusan Mijatovic (dv4all)
+// SPDX-FileCopyrightText: 2022 Jesús García Gonzalez (Netherlands eScience Center) <j.g.gonzalez@esciencecenter.nl>
+// SPDX-FileCopyrightText: 2022 Netherlands eScience Center
+// SPDX-FileCopyrightText: 2022 dv4all
+// SPDX-FileCopyrightText: 2022 Marc Hanisch (GFZ) <marc.hanisch@gfz-potsdam.de>
+// SPDX-FileCopyrightText: 2022 Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences
+//
+// SPDX-License-Identifier: Apache-2.0
+
 /**
  * Colors for both themes
  * Tailwind and MUI-5 based on MUI-5 definitions
